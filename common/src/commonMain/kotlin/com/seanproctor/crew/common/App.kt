9b9cd1b52932f9b3b37173ad7c8a5a9ec331efc7
Sean Proctor <sproctor@gmail.com>
2022-11-04 10:54:53 -0400

    add player/points fields

--- common/src/commonMain/kotlin/com/seanproctor/crew/common/App.kt
+++ common/src/commonMain/kotlin/com/seanproctor/crew/common/App.kt
@@ -1,27 +1,66 @@
 package com.seanproctor.crew.common
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
-import androidx.compose.material.Button
-import androidx.compose.material.Card
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.seanproctor.crew.common.data.CardRepository
+import com.seanproctor.crew.common.data.getPoints
 import com.seanproctor.crew.common.models.Card
 import kotlinx.coroutines.launch
 
 @Composable
 fun App(cardRepository: CardRepository) {
-    val cardsState = remember { mutableStateOf<List<Card>?>(null) }
     val scope = rememberCoroutineScope()
 
-    Column {
-        Button(onClick = {
-            scope.launch {
-                cardsState.value = cardRepository.drawCards(3, 5)
+    Column(Modifier.padding(16.dp)) {
+        var players by remember { mutableStateOf(3) }
+        var playersForDraw by remember { mutableStateOf(3) }
+        val cardsState = remember { mutableStateOf<List<Card>?>(null) }
+        var pointsText by remember { mutableStateOf("1") }
+
+        Row {
+            TextField(
+                value = players.toString(),
+                readOnly = true,
+                onValueChange = {}
+            )
+            Spacer(Modifier.width(8.dp))
+            IconButton(
+                onClick = { if (players > 3) players -= 1 },
+                enabled = players > 3
+            ) {
+                Text("-")
             }
-        }) {
+            Spacer(Modifier.width(8.dp))
+            IconButton(
+                onClick = { if (players < 5) players += 1 },
+                enabled = players < 5
+            ) {
+                Text("+")
+            }
+        }
+        Spacer(Modifier.height(8.dp))
+        TextField(
+            value = pointsText,
+            onValueChange = {
+                pointsText = it
+            }
+        )
+        Spacer(Modifier.height(8.dp))
+        val points = pointsText.toIntOrNull()
+        Button(
+            onClick = {
+                scope.launch {
+                    playersForDraw = players
+                    cardsState.value = cardRepository.drawCards(players, points ?: 0)
+                }
+            },
+            enabled = points != null,
+        ) {
             Text("Draw cards")
         }
 
@@ -30,7 +69,10 @@ fun App(cardRepository: CardRepository) {
             LazyColumn {
                 items(cards) { card ->
                     Card {
-                        Text("Rule: ${card.rule}")
+                        Column {
+                            Text("Points: ${card.getPoints(playersForDraw)}")
+                            Text("Rule: ${card.rule}")
+                        }
                     }
                 }
             }
